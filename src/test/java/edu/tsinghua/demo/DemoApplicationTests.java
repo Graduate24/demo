f4b7bc6ae2c50bdfd036743d57f7e80ba8ca1357
zhangran653 <zhangran42@126.com>
2023-09-21 14:17:15 +0800

    test case

--- src/test/java/edu/tsinghua/demo/DemoApplicationTests.java
+++ src/test/java/edu/tsinghua/demo/DemoApplicationTests.java
@@ -1,13 +1,21 @@
 package edu.tsinghua.demo;
 
+import edu.tsinghua.demo.controller.Controller;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private Controller controller;
+
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
---
+++ src/test/java/edu/tsinghua/demo/HttpRequestTest.java
@@ -0,0 +1,26 @@
+package edu.tsinghua.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class HttpRequestTest {
+
+    @Value(value = "${server.port}")
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject(
+                "http://localhost:" + port + "/test1", String.class))
+                .contains("Hello, World");
+    }
+}
