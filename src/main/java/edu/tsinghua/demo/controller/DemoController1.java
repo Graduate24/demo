0a87fee1556b58b607caa7eac4f7f12ccb5088fe
zhangran653 <zhangran42@126.com>
2023-09-23 12:18:43 +0800

    test case

--- src/main/java/edu/tsinghua/demo/controller/DemoController1.java
+++ src/main/java/edu/tsinghua/demo/controller/DemoController1.java
@@ -1,6 +1,7 @@
 package edu.tsinghua.demo.controller;
 
 import edu.tsinghua.demo.service.demo1.Demo1Service;
+import edu.tsinghua.demo.service.demo1.GoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,23 +18,36 @@ public class DemoController1 {
     @Qualifier("good")
     private Demo1Service service;
 
+    private Demo1Service serviceNew = new GoodService();
+
     @Autowired
     private Map<String, Demo1Service> serviceMap;
 
     @GetMapping("/test1")
     public String test1() {
-        return "Hello, World!";
-    }
-
-    @GetMapping("/test2")
-    public @ResponseBody
-    String test2() {
         return service.serviceOne();
     }
 
+    @GetMapping("/test2")
+    public String test2() {
+        return serviceNew.serviceOne();
+    }
+
     @GetMapping("/test3")
-    public @ResponseBody
-    String test3(@RequestParam String level) {
+    public String test3() {
+        Demo1Service service3 = new GoodService();
+        return service3.serviceTwo();
+    }
+
+    @GetMapping("/test4")
+    public String test4(@RequestParam String level) {
         return serviceMap.get(level).serviceOne();
+    }
+
+    public static void main(String[] args) {
+        DemoController1 controller1 = new DemoController1();
+        controller1.test1();
+        controller1.test2();
+        controller1.test3();
     }
 }
