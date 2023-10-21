1c22bb0dd0959940d21ef2fcef5ecc37c2d7f3e5
zhangran <zhangran42@126.com>
2023-10-21 17:03:57 +0800

    test

--- src/main/java/edu/tsinghua/demo/controller/DemoController1.java
+++ src/main/java/edu/tsinghua/demo/controller/DemoController1.java
@@ -8,6 +8,7 @@ import edu.tsinghua.demo.service.demo1.MediumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
